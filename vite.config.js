d38eab80c1d7bf027c4e25943432dca05e319f1f
ACER <acer@example.com>
2026-05-22 19:50:02 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+})
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
